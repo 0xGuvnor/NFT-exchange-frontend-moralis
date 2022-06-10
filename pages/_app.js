fa8ec89285a958fb950e20ffc8a246c00c465321
0xbagholder <101540063+0xbagholder@users.noreply.github.com>
2022-06-11 00:42:37 +0800

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Head from "next/head";
+import { MoralisProvider } from "react-moralis";
+import Header from "../components/Header";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<div>
+			<Head>
+				<title>Broad Ocean</title>
+				<meta name="description" content="NFT Exchange" />
+				<link rel="icon" href="/skull.png" />
+			</Head>
+
+			<MoralisProvider initializeOnMount={false}>
+				<Header />
+				<Component {...pageProps} />
+			</MoralisProvider>
+		</div>
+	);
 }
 
-export default MyApp
+export default MyApp;
